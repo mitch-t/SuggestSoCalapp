3d4f9310a2d7290861a1695084dcd3ac3276d863
Ru <rudikraeher@gmail.com>
2020-09-21 21:20:59 -0700

    workin on integrating entertainment script

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,80 @@ var queryShopping = "https://api.discountapi.com/v2/deals?" + shopping + "&api_k
 var queryWellness = "https://api.discountapi.com/v2/deals?" + wellness + "&api_key=" + apiKey; 
 var apiKey = "uFVXWRdL";
 var i = -1;
+// var city = document.getElementById("city").value   
+
+////////////////////
+
+{/* <form class="form-inline m-2">
+<input class="form-control mr-sm-2" id="city-search" type="search" placeholder="Search for a city" aria-label="Search">
+<button class="btn btn-outline-success my-2 my-sm-0" id="search-btn" type="submit">Search</button>
+</form> */}
+
+function getEntertainment(i){
+    var city = document.getElementById("city").value 
+    function cityInput(){
+        $("#city").("onsubmit")
+    }
+
+$.ajax({
+    url: "https://app.ticketmaster.com/discovery/v2/events.json?&city="+ city + "&size=10&apikey=TLhN2TG989Us31qJyUNOxHMCWjq9wCYp",
+    method: "GET"
+}).then(function(response){
+    console.log(response)
+    var eventId = response._embedded.events[i].id;
+    $.ajax({
+        url: "https://app.ticketmaster.com/discovery/v2/events/" + eventId + "/images.json?&size=10&apikey=TLhN2TG989Us31qJyUNOxHMCWjq9wCYp",
+        method: "GET",
+    }).then(function(responseImg){
+        console.log(responseImg)
+    var eventName = response._embedded.events[i].name;
+    var eventSales = response._embedded.events[i].url;
+    var eventPicture = responseImg.images[i].url;
+    var eventNameDisplayed = JSON.stringify(eventName);
+    var eventSalesLink = JSON.stringify(eventSales);
+    console.log(eventSalesLink);
+ 
+        // This callback sets the page content
+        function displayNewDeal(){
+            $(".entertainment #text").text(eventNameDisplayed);
+            $("#img3").attr({src: eventPicture, alt: "event image"});
+            $(".entertainment #link").attr("href", eventSalesLink);
+        };
+        displayNewDeal();
+    });
+});
+};
+
+// $("#city").on("click", function () {
+//     i++
+//     getEntertainment(i);
+// })
+
+// This click handler allows the user to get a new event. 
+$(".entertainment #next").on("click", function(event){
+    event.preventDefault();
+    i++;
+    if (i > 10){
+        i = 0;
+        getEntertainment(i);
+    } else {
+        getEntertainment(i);
+    };
+});
+
+// This click event allows the user to go back one event at a time.
+$(".entertainment #back").on("click", function(event){
+    event.preventDefault();
+    i--;
+    if (i < 0){
+        i = 10;
+        getEntertainment(i);
+    } else {
+        getEntertainment(i);
+    };
+});
+
+/////////////////////////////////////////////////
 
 // This is the function to get wellness deals
 function getWell(i){
